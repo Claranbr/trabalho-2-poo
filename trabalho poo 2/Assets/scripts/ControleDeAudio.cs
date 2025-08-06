c6209552020ce1d2fb03da851210eb797751dc13
Claranbr <clara.nobre@escolar.ifrn.edu.br>
2025-08-06 08:31:56 -0300

    agr

--- trabalho poo 2/Assets/scripts/ControleDeAudio.cs
+++ trabalho poo 2/Assets/scripts/ControleDeAudio.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,15 +12,32 @@ public class ControleDeAudio : MonoBehaviour
     public Slider volumeSlider;
     public Slider VolumeSFXSlider;
     public Toggle toggleMusica;
-    
+    public TMP_Text textoMusica;
     void Start()
     {
         musica = toggleMusica.isOn;
+        Volume = (int)volumeSlider.value;
+        VolumeSFX = (int)VolumeSFXSlider.value;
     }
 
 
     void Update()
     {
+        musica = toggleMusica.isOn;
+        Volume = (int)volumeSlider.value;
+        VolumeSFX = (int)VolumeSFXSlider.value;
+
+        if (musica == true)
+        {
+            textoMusica.text = "Ligado";
+            textoMusica.color = Color.green;
+        }
+        else
+        {
+            textoMusica.text = "Desligado";
+            textoMusica.color = Color. red;
+            
+        }
         
     }
 }
