452fd2e92abe7c3f76e7d6f3b4486c53893fe292
Claranbr <clara.nobre@escolar.ifrn.edu.br>
2025-08-08 16:12:22 -0300

    lab

---
+++ trabalho poo 2/Assets/scripts/Personagem.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+
+    private int vidas;
+    private int energia;
+
+
+    public void setvidas(int vidas)
+    {
+        this.vidas = vidas;
+    }
+
+    public int getvidas()
+    {
+        return this.vidas;
+    }
+
+    public void setEnergia(int energia)
+    {
+        this.energia = energia;
+    }
+    
+    public int
